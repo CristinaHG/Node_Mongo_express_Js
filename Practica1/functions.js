77c5f38183b4237961d4eb679a39be44d8ff621d
CristinaHG <mrcrstnherediagmez@gmail.com>
2017-11-24 22:38:27 +0100

    function examples

---
+++ Practica1/functions.js
@@ -0,0 +1,63 @@
+
+// basic function
+
+function basic() {
+    console.log("im so basic!")
+}
+
+basic()
+
+// function as declaration with arguments and return
+function add(a,b) {
+    return a+b
+}
+
+console.log(add(2,3))
+
+//function as expression
+
+var multiply=function (a,b) {
+    return a*b
+}
+
+console.log(multiply(2,3))
+
+var lang='en'
+
+function contextAccess() {
+    console.log("the context language is:" +lang)
+}
+
+contextAccess()
+
+//method of an object
+
+var agent={
+    name:"louis",
+    sayhello: function () {
+        console.log("hello from "+this.name)
+    }
+}
+
+agent.sayhello()
+
+
+// object constructor
+
+function Fruit(name) {
+    this.getName=function () {
+        return name
+    }
+    this.setName=function (value) {
+        name=value
+    }
+}
+
+// create fruit object
+
+var orange=new Fruit("Orange")
+
+console.log(orange)
+console.log(orange.getName())
+orange.setName('mandarin')
+console.log(orange.getName())
