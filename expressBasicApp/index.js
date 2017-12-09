3c763c68b711344404e5d5cb6dc27ec0715762a8
CristinaHG <mrcrstnherediagmez@gmail.com>
2017-12-09 21:07:10 +0100

    doing basic express web app

---
+++ expressBasicApp/index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+//load modules
+var express=require('express')
+var morgan=require('morgan')
+
+
+var app=express() // function that creates express App when called
+app.use(morgan('dev')) //tells express App to use morgan
+
+app.get('/',function (request,response) {
+    console.log("petition to /")
+    response.send('Hello world')
+})
+
+var server=app.listen(3000, function () {
+    var port=server.address().port
+    console.log("server running at port: " , port)
+})
