d9cd58667ef37b0e45506ab35e5bfa9b876d01f8
CristinaHG <mrcrstnherediagmez@gmail.com>
2017-12-09 20:13:19 +0100

    adding node modules example

---
+++ modules/addition.js
@@ -0,0 +1,14 @@
+'use strict'
+
+var add=function (a,b) {
+    return a+b
+}
+
+var substract=function (a,b) {
+    return a-b
+}
+
+module.exports={
+    addition:add,
+    substraction:substract
+}
---
+++ modules/index.js
@@ -0,0 +1,6 @@
+'use strict'
+
+var add = require('./addition')
+
+console.log(add.addition(5,7))
+console.log(add.substraction(5,7))
